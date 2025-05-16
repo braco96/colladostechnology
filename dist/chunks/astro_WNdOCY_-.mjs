046c458c47923585514bb331e1abb5d96341319a
braco96 <bravocolladoluis@gmail.com>
2025-05-16 14:16:48 +0200

    nuevo2

--- dist/chunks/astro_WNdOCY_-.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_CQl11rBO.mjs';
-import 'clsx';
--- dist/pages/_---slug_.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { p as page } from '../chunks/index_1S5Jujy-.mjs';
-export { renderers } from '../renderers.mjs';
